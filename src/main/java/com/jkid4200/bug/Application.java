182874d1ea3a08143a92abe4a733ff8a69c31843
Mike Nau <mikenau75@gmail.com>
2021-11-03 18:08:23 -0500

    Initial commit.

---
+++ src/main/java/com/jkid4200/bug/Application.java
@@ -0,0 +1,11 @@
+package com.jkid4200.bug;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+
+  public static void main(String[] args) {
+    log.info("Hello world!");
+  }
+}
---
+++ src/main/java/com/jkid4200/bug/engine/Window.java
@@ -0,0 +1,11 @@
+package com.jkid4200.bug.engine;
+
+import lombok.Data;
+
+@Data
+public class Window {
+
+  private String title;
+  private int width;
+  private int height;
+}
